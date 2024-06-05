2163dcda4028f0c01346ae62361869718035190c
A very unfunny developer <74575128+GitPoopHub@users.noreply.github.com>
2024-06-05 16:13:27 -0700

    Update dyertool.js

--- dyertool.js
+++ dyertool.js
@@ -1,5 +1,7 @@
 addTool("dyer", {
-    func: function(placed) { placed.h = random(0-255); .s = random(0-255); .l = random(0-255); },
+    func: function(placed) { placed.h = random(0-255); },
+    func: function(placed) { placed.s = random(0-255); },
+    func: function(placed) { placed.l = random(0-255); },
     onSelect: function() { alert("Use on an ingredient to dye it any color!"); },
     whileOn: function() { console.log("Dyer selected."); },
     onDeselect: function() { alert("Goodbye!"); },
