1d8f3f5bef9b097b4941bebf2c97aba5e3732159
A very unfunny developer <74575128+GitPoopHub@users.noreply.github.com>
2024-06-05 15:58:44 -0700

    Create dyer tool.js

---
+++ dyer tool.js
@@ -0,0 +1,9 @@
+addTool("dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it any color!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+})
