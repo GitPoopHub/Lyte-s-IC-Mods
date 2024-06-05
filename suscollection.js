7ca9fe2f245f613f15f276383e25f3c3b3d87532
A very unfunny developer <74575128+GitPoopHub@users.noreply.github.com>
2024-06-05 15:44:22 -0700

    Create suscollection.js

---
+++ suscollection.js
@@ -0,0 +1,62 @@
+{
+  ingredients.semen = {
+    type: "liquid",
+    color: [ "#d6d6cb" ],
+    keywords: 'semen,cum,sperm,jizz,spunk,nut,baby batter,cream',
+    dishWeight: -55,
+    adj: "cummy",
+    "h": 60,
+    "s": 12,
+    "l": 82,
+    hidden:true,
+    "shape": "liquid"
+  };
+
+  ingredients.poop = {
+    shape:"blob",
+    type:"paste",
+    color:["#5c3508"],
+    h: 32,
+    s: 84,
+    l: 20,
+    adj:"shitty",
+    hidden:true,
+    keywords: 'doo,poo,poop,poopoo,shit,crap,feces,scat,dung,number 2,#2'
+  };
+  ingredients.vomit = {
+    type: "paste",
+    color: [ "#56a637" ],
+    keywords: 'puke,barf,vomit,upcheck,throw up,chunder,hurl',
+    adj: "barfy",
+    "h": 65,
+    "s": 45,
+    "l": 23,
+    hidden:true,
+    "shape": "liquid"
+  };
+
+  ingredients.pee = {
+    shape:"liquid",
+    type:"liquid",
+    color:["ffff00"],
+    h: 32,
+    s: 84,
+    l: 20,
+    adj:"pissy",
+    hidden:true,
+    keywords: 'wee,pee,peepee,piss,urine,tinkle,omorashi,wet,number 1,#1'
+  };
+  
+  ingredients.snot = {
+    type: "powder",
+    color: [ "#7ea170" ],
+    keywords: 'snot,booger,mucus
+    dishWeight: -55,
+    adj: "boogery",
+    "h": 60,
+    "s": 12,
+    "l": 82,
+    hidden:true,
+    "shape": "rock"
+  }
+}
