40fbb8e7e5bd7ffa10c1998bae3a291df848023a
A very unfunny developer <74575128+GitPoopHub@users.noreply.github.com>
2024-06-05 20:50:41 -0400

    Create mercury.js

---
+++ mercury.js
@@ -0,0 +1,20 @@
+(
+addIngredient("mercury", {
+        shape:"liquid",
+        color:["#C4C4C4"],
+        innerColor:["#FFFFFF"],
+        adj:"toxic",
+        type:"liquid"
+        hidden:true
+    }
+);
+(
+addIngredient("mercury laxative", {
+        shape:"liquid",
+        color:["#C4C4C4"],
+        innerColor:["#FFFFFF"],
+        adj:"toxic",
+        type:"thick_liquid"
+        hidden:true
+    }
+);
